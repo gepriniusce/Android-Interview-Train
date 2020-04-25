c997f6fc4c3ad496be223b16e00554efdcb46947
Tongson <tongsonloo@gmail.com>
2020-04-25 17:06:58 +0800

    播放器

---
+++ train_video/src/main/java/pr/tongson/train_video/VideoActivity.kt
@@ -0,0 +1,12 @@
+package pr.tongson.train_video
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class VideoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video)
+    }
+}
